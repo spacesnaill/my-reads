fef5a98e876588f41a8dd9f3a1e0fae393e069e4
Patrick Geiger <patrickjgeiger@gmail.com>
2019-07-24 19:06:53 -0400

    feat: added basic components to BookList, no data yet

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,1 +1,20 @@
 import React from "react";
+import Book from "./Book";
+import MoveBook from "./MoveBook";
+
+function BookList(props) {
+  return (
+    <ol>
+      {props.bookShelf.map(book => {
+        return (
+          <li>
+            <Book />
+            <MoveBook />
+          </li>
+        );
+      })}
+    </ol>
+  );
+}
+
+export default BookList;
