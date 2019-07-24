b63eb8477b4b41cb978a0aae5483a93a24c3cd1b
Patrick Geiger <patrickjgeiger@gmail.com>
2019-07-24 19:11:25 -0400

    feat: added basic Book component requirements and proptypes

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,1 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+function Book(props) {
+  return (
+    <div>
+      <img alt="{props.title} Book Cover" src={props.image} />
+      <p>{props.title}</p>
+    </div>
+  );
+}
+
+Book.prototype = {
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired
+};
+
+export default Book;
