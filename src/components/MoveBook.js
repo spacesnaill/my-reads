aca548d52bbbb1a9104a08cbbbf782ff88b89ee6
Patrick Geiger <patrickjgeiger@gmail.com>
2019-07-29 17:46:15 -0400

    Revert "feat: added dropdown menus for MoveBook"
    This reverts commit 1f9dca570353ce2bfafd556ebd3049efe312066b.

--- src/components/MoveBook.js
+++ src/components/MoveBook.js
@@ -1,41 +1,7 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
 
 function MoveBook(props) {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  function handleClick(event) {
-    setAnchorEl(event.currentTarget);
-  }
-
-  function handleClose() {
-    setAnchorEl(null);
-  }
-
-  return (
-    <div>
-      <Button
-        aria-controls="simple-menu"
-        aria-haspopup="true"
-        onClick={handleClick}
-      >
-        Open Menu
-      </Button>
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose}>Want to Read</MenuItem>
-        <MenuItem onClick={handleClose}>Currently Reading</MenuItem>
-        <MenuItem onClick={handleClose}>Read</MenuItem>
-      </Menu>
-    </div>
-  );
+  return <button>Move Book</button>;
 }
 
 export default MoveBook;
