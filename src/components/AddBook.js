7b01201eb31467fd5f08d8aaf009c436dd860da0
Patrick Geiger <patrickjgeiger@gmail.com>
2019-08-01 14:38:28 -0400

    feat: Add Book button now takes you to the page to search for books

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 function AddBook(props) {
-  return <Button className="add-book-button">&#43; Add Book</Button>;
+  return (
+    <Link to="/BookSearch">
+      <Button className="add-book-button">&#43; Add Book</Button>
+    </Link>
+  );
 }
 
 export default AddBook;
