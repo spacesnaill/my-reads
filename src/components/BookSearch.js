eb856913275198b82d77583b81363e22fc1adf19
Patrick Geiger <patrickjgeiger@gmail.com>
2019-08-06 12:16:59 -0400

    feat: made the search box larger and removed button

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -40,22 +40,32 @@ class BookSearch extends Component {
     });
   };
 
+  moveBookToShelf = (book, bookShelfName) => {
+    BooksApi.update(book, bookShelfName).then(() => {
+      this.updateUIBookShelf(bookShelfName, book.id);
+    });
+  };
+
   render() {
     return (
       <Container width="66%" textAlign="center">
-        <Segment textAlign="center" className="" loading={this.state.loading}>
+        <Segment textAlign="center" loading={this.state.loading}>
           <Header textAlign="left" as="h1">
             Book Search
           </Header>
-          <form>
-            <Input
-              value={this.state.searchValue}
-              placeholder="Search for books"
-              onChange={this.onSearchInputChange}
-              action={{ content: "Search" }}
-            />
-          </form>
-          <BookList bookShelf={this.state.searchResults} shelfName="" />
+          <Input
+            className="book-search-input"
+            value={this.state.searchValue}
+            placeholder="Search for books"
+            onChange={this.onSearchInputChange}
+            size="large"
+            icon="book"
+          />
+          <BookList
+            bookShelf={this.state.searchResults}
+            moveBookToShelf={this.moveBookToShelf}
+            shelfName=""
+          />
         </Segment>
         <Link to="/">
           <Button className="back-button">&#8592; Back to Bookshelves</Button>
