6d9f850f1188df220c2ad7b5f102600f28c07478
Patrick Geiger <patrickjgeiger@gmail.com>
2019-08-06 10:48:49 -0400

    refactor: moved the "MoveBook" functionality into its own component
    this allows for changes to be made easier. For example if another shelf needs to be added it should be much simpler to add it on as opposed to how it was before

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,9 +2,15 @@ import React from "react";
 import Book from "./Book";
 import MoveBook from "./MoveBook";
 import PropTypes from "prop-types";
-import { Card, Dropdown } from "semantic-ui-react";
+import { Card } from "semantic-ui-react";
 
 function BookList(props) {
+  const bookShelfOptions = [
+    { name: "wantToRead", title: "Want To Read" },
+    { name: "currentlyReading", title: "Currently Reading" },
+    { name: "read", title: "Read" }
+  ];
+
   return (
     <Card.Group className="bookList">
       {props.bookShelf.map(book => {
@@ -19,42 +25,11 @@ function BookList(props) {
               }
             />
             <Card.Content extra>
-              <Dropdown text="Move Book to...">
-                <Dropdown.Menu>
-                  <Dropdown.Item
-                    disabled={props.shelfName === "wantToRead"}
-                    onClick={event => {
-                      props.moveBookToShelf(
-                        book,
-                        props.shelfName,
-                        "wantToRead"
-                      );
-                    }}
-                  >
-                    Want To Read
-                  </Dropdown.Item>
-                  <Dropdown.Item
-                    disabled={props.shelfName === "currentlyReading"}
-                    onClick={event => {
-                      props.moveBookToShelf(
-                        book,
-                        props.shelfName,
-                        "currentlyReading"
-                      );
-                    }}
-                  >
-                    Currently Reading
-                  </Dropdown.Item>
-                  <Dropdown.Item
-                    disabled={props.shelfName === "read"}
-                    onClick={event => {
-                      props.moveBookToShelf(book, props.shelfName, "read");
-                    }}
-                  >
-                    Read
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
+              <MoveBook
+                bookShelves={bookShelfOptions}
+                shelfName={book.shelf}
+                book={book}
+              />
             </Card.Content>
           </Card>
         );
@@ -66,11 +41,7 @@ function BookList(props) {
 BookList.propTypes = {
   bookShelf: PropTypes.array.isRequired,
   shelfName: PropTypes.string.isRequired,
-  moveBookToShelf: PropTypes.func.isRequired
+  moveBookToShelf: PropTypes.func
 };
 
 export default BookList;
-// onClick={event => {
-//   event.preventDefault();
-//   props.moveBookToShelf(book.id, props.shelfName, "read");
-// }}
--- src/components/MoveBook.js
+++ src/components/MoveBook.js
@@ -1,8 +1,27 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
-
+import { Dropdown } from "semantic-ui-react";
+import { update } from "../BooksApi";
 function MoveBook(props) {
-  return <Button>Move Book</Button>;
+  function moveBookToShelf(book, shelfName, runAfterUpdate) {
+    update(book, shelfName);
+  }
+
+  return (
+    <Dropdown text="Move Book to. . .">
+      <Dropdown.Menu>
+        {props.bookShelves.map(bookShelf => {
+          return (
+            <Dropdown.Item
+              key={bookShelf.name}
+              disabled={props.shelfName === bookShelf.name}
+            >
+              {bookShelf.title}
+            </Dropdown.Item>
+          );
+        })}
+      </Dropdown.Menu>
+    </Dropdown>
+  );
 }
 
 export default MoveBook;
