c4539c681fc0a6fda521a96f582772034a5671ce
Patrick Geiger <patrickjgeiger@gmail.com>
2019-08-06 06:51:24 -0400

    feat: performs a search and has back button
    the search is currently static, the user cannot specify what to search, so this is more or less just a proof of concept for now and the actual search functionality will come later

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import { Button, Search } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import BookList from "./BookList";
+import * as BooksApi from "../BooksApi";
 
-function BookSearch(props) {
-  return <h1>Book Search</h1>;
+class BookSearch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: []
+    };
+  }
+
+  componentDidMount() {
+    BooksApi.search("Linux").then(results => {
+      console.log(results);
+      this.setState({
+        searchResults: results
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Book Search</h1>
+        <BookList bookShelf={this.state.searchResults} shelfName="" />
+        <Link to="/">
+          <Button>&#8592; Back to Bookshelves</Button>
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default BookSearch;
