e0e7b5165fa5b5ab110c5d92cdfadc79ed39965e
Patrick Geiger <patrickjgeiger@gmail.com>
2019-08-07 12:46:00 -0400

    refactor: moved state from BookShelves to App
    this is because both BookShelves and the BookSearch components will need to share this state, so it needed to be moved up to the parent of both components. This also included turning BookShelves into a stateless functional component as it no longer needs to hold onto any state.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,105 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import BookShelves from "./components/BookShelves";
 import BookSearch from "./components/BookSearch";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import * as BooksApi from "./BooksApi";
 
 // TODO Pick up the state for the bookshelves and put it in this component so it can be shared between both the bookshelves page and the search page
-function App() {
-  return (
-    <Router>
-      <Route exact path="/" component={BookShelves} />
-      <Route exact path="/Search" component={BookSearch} />
-    </Router>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentlyReading: [],
+      wantToRead: [],
+      read: [],
+      loading: true
+    };
+  }
+
+  filterBooksByShelf(books, shelfName) {
+    return books.filter(book => {
+      if (book.shelf === shelfName) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  }
+
+  componentDidMount() {
+    BooksApi.getAll().then(books => {
+      this.setState({
+        currentlyReading: this.filterBooksByShelf(books, "currentlyReading"),
+        wantToRead: this.filterBooksByShelf(books, "wantToRead"),
+        read: this.filterBooksByShelf(books, "read"),
+        loading: false
+      });
+    });
+  }
+
+  deleteBookFromShelf = (bookShelfName, bookId) => {
+    this.setState(currentState => {
+      const newBookShelf = currentState[bookShelfName].filter(book => {
+        if (book.id !== bookId) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+      return {
+        [bookShelfName]: newBookShelf
+      };
+    });
+  };
+
+  addBookToShelf = (bookShelfName, bookId) => {
+    BooksApi.get(bookId).then(book => {
+      this.setState(currentState => {
+        currentState[bookShelfName].push(book);
+        return {
+          [bookShelfName]: currentState[bookShelfName]
+        };
+      });
+    });
+  };
+
+  moveBookToShelf = parameters => {
+    this.addBookToShelf(parameters.destination, parameters.book.id);
+    this.deleteBookFromShelf(parameters.origin, parameters.book.id);
+  };
+
+  render() {
+    return (
+      <Router>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <BookShelves
+              currentlyReading={this.state.currentlyReading}
+              wantToRead={this.state.wantToRead}
+              read={this.state.read}
+              moveBookToShelf={this.moveBookToShelf}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/Search"
+          render={() => (
+            <BookSearch
+              currentlyReading={this.state.currentlyReading}
+              wantToRead={this.state.wantToRead}
+              read={this.state.read}
+            />
+          )}
+        />
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/BookShelves.js
+++ src/components/BookShelves.js
@@ -1,126 +1,60 @@
 import React, { Component } from "react";
-import * as BooksApi from "../BooksApi";
 import BookList from "./BookList";
 import AddBook from "./AddBook";
 import { Segment, Container, Header, Menu } from "semantic-ui-react";
 
-class BookShelves extends Component {
-  constructor(props) {
-    super(props);
+function BookShelves(props) {
+  return (
+    <Container width="66%" textAlign="center">
+      <Segment.Group>
+        <Segment
+          textAlign="center"
+          className="bookListContainer"
+          loading={props.loading}
+        >
+          <Header textAlign="left" as="h2">
+            Want To Read
+          </Header>
+          <BookList
+            bookShelf={props.wantToRead}
+            runAfterBookIsUpdated={props.moveBookToShelf}
+            shelfName="wantToRead"
+          />
+        </Segment>
 
-    this.state = {
-      currentlyReading: [],
-      wantToRead: [],
-      read: [],
-      loading: true
-    };
-  }
+        <Segment
+          textAlign="center"
+          className="bookListContainer"
+          loading={props.loading}
+        >
+          <Header textAlign="left" as="h2">
+            Currently Reading
+          </Header>
+          <BookList
+            bookShelf={props.currentlyReading}
+            runAfterBookIsUpdated={props.moveBookToShelf}
+            shelfName="currentlyReading"
+          />
+        </Segment>
 
-  filterBooksByShelf(books, shelfName) {
-    return books.filter(book => {
-      if (book.shelf === shelfName) {
-        return true;
-      } else {
-        return false;
-      }
-    });
-  }
-
-  componentDidMount() {
-    BooksApi.getAll().then(books => {
-      this.setState({
-        currentlyReading: this.filterBooksByShelf(books, "currentlyReading"),
-        wantToRead: this.filterBooksByShelf(books, "wantToRead"),
-        read: this.filterBooksByShelf(books, "read"),
-        loading: false
-      });
-    });
-  }
-
-  deleteBookFromShelf = (bookShelfName, bookId) => {
-    this.setState(currentState => {
-      const newBookShelf = currentState[bookShelfName].filter(book => {
-        if (book.id !== bookId) {
-          return true;
-        } else {
-          return false;
-        }
-      });
-      return {
-        [bookShelfName]: newBookShelf
-      };
-    });
-  };
-
-  addBookToShelf = (bookShelfName, bookId) => {
-    BooksApi.get(bookId).then(book => {
-      this.setState(currentState => {
-        currentState[bookShelfName].push(book);
-        return {
-          [bookShelfName]: currentState[bookShelfName]
-        };
-      });
-    });
-  };
-
-  moveBookToShelf = parameters => {
-    this.addBookToShelf(parameters.destination, parameters.book.id);
-    this.deleteBookFromShelf(parameters.origin, parameters.book.id);
-  };
-
-  render() {
-    return (
-      <Container width="66%" textAlign="center">
-        <Segment.Group>
-          <Segment
-            textAlign="center"
-            className="bookListContainer"
-            loading={this.state.loading}
-          >
-            <Header textAlign="left" as="h2">
-              Want To Read
-            </Header>
-            <BookList
-              bookShelf={this.state.wantToRead}
-              runAfterBookIsUpdated={this.moveBookToShelf}
-              shelfName="wantToRead"
-            />
-          </Segment>
-
-          <Segment
-            textAlign="center"
-            className="bookListContainer"
-            loading={this.state.loading}
-          >
-            <Header textAlign="left" as="h2">
-              Currently Reading
-            </Header>
-            <BookList
-              bookShelf={this.state.currentlyReading}
-              runAfterBookIsUpdated={this.moveBookToShelf}
-              shelfName="currentlyReading"
-            />
-          </Segment>
-
-          <Segment
-            textAlign="center"
-            className="bookListContainer"
-            loading={this.state.loading}
-          >
-            <Header textAlign="left" as="h2">
-              Read
-            </Header>
-            <BookList
-              bookShelf={this.state.read}
-              runAfterBookIsUpdated={this.moveBookToShelf}
-              shelfName="read"
-            />
-          </Segment>
-          <AddBook />
-        </Segment.Group>
-      </Container>
-    );
-  }
+        <Segment
+          textAlign="center"
+          className="bookListContainer"
+          loading={props.loading}
+        >
+          <Header textAlign="left" as="h2">
+            Read
+          </Header>
+          <BookList
+            bookShelf={props.read}
+            runAfterBookIsUpdated={props.moveBookToShelf}
+            shelfName="read"
+          />
+        </Segment>
+        <AddBook />
+      </Segment.Group>
+    </Container>
+  );
 }
 
 export default BookShelves;
